21efc62938484391041f262aa089356cf24aedca
yungstunna241 <truemudbaby@gmail.com>
2025-07-11 06:28:29 +0400

    removed login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './Login'
-import MainApp from './MainApp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainApp from './MainApp';
 
 export default function App() {
-  const token = localStorage.getItem('token')
-
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/login"
-          element={token ? <Navigate to="/" replace /> : <Login />}
-        />
-        <Route
-          path="/*"
-          element={token ? <MainApp /> : <Navigate to="/login" replace />}
-        />
+        <Route path="/*" element={<MainApp />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
