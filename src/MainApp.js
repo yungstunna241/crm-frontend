74a53235abfd70c87fb57173a4a3fc07febff202
yungstunna241 <truemudbaby@gmail.com>
2025-07-11 14:23:02 +0400

    t cgit commit t cgit commit git commit t cgit commit

--- src/MainApp.js
+++ src/MainApp.js
@@ -21,7 +21,7 @@ const [kycInput, setKycInput] = useState('');
 const agentName = localStorage.getItem('role') || 'Agent';
 
 const updateStatus = (id, newStatus) => {
-  axios.post(`https://crm-backend-1-2mb5.onrender.com/contacts/${id}/update`, { result: newStatus })
+  axios.post(`https://crm-backend20.onrender.com/contacts/${id}/update`, { result: newStatus })
     .then(() => {
       setContacts(prev => prev.map(c => c._id === id ? { ...c, result: newStatus } : c));
     })
@@ -29,9 +29,9 @@ const updateStatus = (id, newStatus) => {
 };
 
 useEffect(() => {
-  axios.get('https://crm-backend-1-2mb5.onrender.com')
+  axios.get('https://crm-backend20.onrender.com/contacts')
     .then(res => setContacts(res.data))
-    .catch(() => alert('Cannot reach backend'));
+    .catch(() => alert('Cannot reach backends'));
 }, []);
 const filteredContacts = contacts.filter(c => {
   const statusMatch =
@@ -281,7 +281,7 @@ const filteredContacts = contacts.filter(c => {
         <button
           onClick={() => {
             const agent = localStorage.getItem('role') || 'Agent';
-            axios.post(`https://crm-backend-1-2mb5.onrender.com/contacts/${currentContact._id}/update`, { kyc: `${kycInput} (by ${agent})` })
+            axios.post(`https://crm-backend20.onrender.com/contacts/${currentContact._id}/update`, { kyc: `${kycInput} (by ${agent})` })
               .then(() => {
                 setContacts(prev => prev.map(c => c._id === currentContact._id ? { ...c, kyc: `${kycInput} (by ${agent})` } : c));
                 setShowKYC(false);
